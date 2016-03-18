0c39a9609f3601575ddb33609ac415faa4e8876c
danieladler <weston.adler@gmail.com>
2016-03-18 16:00:45 -0700

    continue refactoring home, preparation save and validation

--- app/models/home_assessment.rb
+++ app/models/home_assessment.rb
@@ -11,119 +11,116 @@ class HomeAssessment
 
   def new_preparation
     # @preparation = home.preparations.new(user_id: user_id)
-    @preparation = Preparation.new(user_id: user_id, home_id: home_id)
+    @preparation = Preparation.new(current_user.id, home.id)
   end
 
   def create_and_check(keyword)
     new_preparation
     @preparation.question = Question.find_by(keyword: keyword)
-    #
+    @preparation.save
   end
 
   def run_all_checks
-    # array of keywords
-    #  arr.each create_and_check |k| 
+    common_keywords = %w(furniture water_heater gas_heater hanging decor cabinets
+    gas_shutoff water_shutoff electricity_shutoff shutoff_tool extinguisher
+    flammable_liquids)
 
-    anchor_furniture
-    anchor_water_heater
-    anchor_gas_heater
-    safe_hanging_audit
-    safe_decor_audit
-    latch_cabinets
-    id_gas_shutoff
-    id_water_shutoff
-    id_electricity_shutoff
-    shutoff_tool
-    flam_liquids_outside
-    if home.is_house?
-      flexible_utility_pipes
-      anchor_house_to_foundation
+    house_only_keywords = %w(pipes foundation)
+
+    common_keywords.each do |k|
+      create_and_check(k)
+    end
+
+    if self.home.is_house?
+      house_only_keywords.each do |k|
+        create_and_check(k)
+      end
     end
   end
 
   # Checks that apply to both house and apartment dwellers
-  def anchor_furniture
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"furniture")
-    @preparation.save
-  end
-
-  def anchor_water_heater
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"water_heater")
-    @preparation.save
-  end
-
-  def anchor_gas_heater
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"gas_heater")
-    @preparation.save
-  end
-
-  def safe_hanging_audit
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"hanging")
-    @preparation.save
-  end
-
-  def safe_decor_audit
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"decor")
-    @preparation.save
-  end
-
-  def latch_cabinets
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"cabinets")
-    @preparation.save
-  end
-
-  def id_gas_shutoff
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"gas_shutoff")
-    @preparation.save
-  end
-
-  def id_water_shutoff
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"water_shutoff")
-    @preparation.save
-  end
-
-  def id_electricity_shutoff
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"electricity_shutoff")
-    @preparation.save
-  end
-
-  def shutoff_tool
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"shutoff_tool")
-    @preparation.save
-  end
-
-  def fire_extinguisher
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"extinguisher")
-    @preparation.save
-  end
-
-  def flam_liquids_outside
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"flammable_liquids")
-    @preparation.save
-  end
-
-  # Checks that apply only to house dwellers
-  def flexible_utility_pipes
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"pipes")
-    @preparation.save
-  end
-
-  def anchor_house_to_foundation
-    new_preparation
-    @preparation.question = Question.find_by(keyword:"foundation")
-    @preparation.save
-  end
+  # def anchor_furniture
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"furniture")
+  #   @preparation.save
+  # end
+  #
+  # def anchor_water_heater
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"water_heater")
+  #   @preparation.save
+  # end
+  #
+  # def anchor_gas_heater
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"gas_heater")
+  #   @preparation.save
+  # end
+  #
+  # def safe_hanging_audit
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"hanging")
+  #   @preparation.save
+  # end
+  #
+  # def safe_decor_audit
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"decor")
+  #   @preparation.save
+  # end
+  #
+  # def latch_cabinets
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"cabinets")
+  #   @preparation.save
+  # end
+  #
+  # def id_gas_shutoff
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"gas_shutoff")
+  #   @preparation.save
+  # end
+  #
+  # def id_water_shutoff
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"water_shutoff")
+  #   @preparation.save
+  # end
+  #
+  # def id_electricity_shutoff
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"electricity_shutoff")
+  #   @preparation.save
+  # end
+  #
+  # def shutoff_tool
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"shutoff_tool")
+  #   @preparation.save
+  # end
+  #
+  # def fire_extinguisher
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"extinguisher")
+  #   @preparation.save
+  # end
+  #
+  # def flam_liquids_outside
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"flammable_liquids")
+  #   @preparation.save
+  # end
+  #
+  # # Checks that apply only to house dwellers
+  # def flexible_utility_pipes
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"pipes")
+  #   @preparation.save
+  # end
+  #
+  # def anchor_house_to_foundation
+  #   new_preparation
+  #   @preparation.question = Question.find_by(keyword:"foundation")
+  #   @preparation.save
+  # end
 end
--- app/models/preparation.rb
+++ app/models/preparation.rb
@@ -1,7 +1,14 @@
 class Preparation < ActiveRecord::Base
-  validates_presence_of :user_id
-  validates :user_id, :uniqueness => { :scope => [:question_id] }
+  # validates_presence_of :user_id
+  # validates :user_id, :uniqueness => { :scope => [:question_id] }
 
   belongs_to :prep, polymorphic: true
   belongs_to :question
+
+  attr_accessor :user_id, :home_id
+
+  def initialize(user_id, home_id)
+    @user_id = user_id
+    @home_id = home_id
+  end
 end
