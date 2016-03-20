715032510151ebc631837577f7670513003b8830
danieladler <weston.adler@gmail.com>
2016-03-20 09:47:22 -0700

    start redoing style and routing for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root to: "app_home#index"
 
-  post "/sign-in" ,                   to: "sessions#sign_in" ,    as: :sign_in
-  get  "/sign-out",                   to: "sessions#sign_out",    as: :sign_out
+  post "/sign-in" ,                   to: "sessions#sign_in" ,             as: :sign_in
+  get  "/sign-out",                   to: "sessions#sign_out",             as: :sign_out
 
-  get "/sign-up",                     to: "users#sign_up",        as: :sign_up
+  get "/sign-up",                     to: "users#sign_up",                 as: :sign_up
 
   get  "/assessment"                  , to: "assessment#assessment_form" , as: :assessment_form
   post "/assessment/update_home"      , to: "home_assessment#update_home", as: :update_home
   post "/assessment/update_dependents", to: "dependent_assessment#update_dependents", as: :update_dependents
   # post: update_contacts
 
-  get "/todo" , to:"todo#index"
+  get "/todo"                         , to:"todo#index",                    as: :todo
 
 end
