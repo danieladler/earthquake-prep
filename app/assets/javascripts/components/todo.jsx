5351e855a6fd08a266e44a90f7b02404e4318abc
danieladler <weston.adler@gmail.com>
2016-03-22 15:25:21 -0700

    mock up list of hard-coded sample preps displayed w/ React

--- app/assets/javascripts/components/todo.jsx
+++ app/assets/javascripts/components/todo.jsx
@@ -1,18 +1,55 @@
-var TestDiv = React.createClass({
+var preps = [
+  {prep_type:"Home", question_contents: "Make sure your home is securely anchored to its foundation"},
+  {prep_type:"Home", question_contents: "Bolt and brace heavy furniture to wall studs"},
+  {prep_type:"Home", question_contents: "Identify location of gas shutoff and how to turn it off."}
+]
+
+var Todo = React.createClass({
+  getInitialState: function() {
+    return {preps: preps, currentPrep: null}
+  },
+  setCurrentTodo: function(prep) {
+    this.setState({currentPrep: prep})
+  },
   render: function() {
     return(
       <div>
-        <h1>Test!</h1>
-        <h1>Test!</h1>
-        <h1>Test!</h1>
+        <PrepTypeList preps={this.state.preps}/>
       </div>
     );
   }
 });
 
+var PrepTypeList = React.createClass({
+  render: function() {
+    var prepDivs = [];
+
+    for (var i = 0; i < this.props.preps.length; i++) {
+      prepDivs.push(<PrepItem key={i} prep={this.props.preps[i]}/>)
+    }
+
+    return(
+      <div className="prep-type-list">
+        {prepDivs}
+      </div>
+    )
+  }
+
+});
+
+var PrepItem = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <p>{this.props.prep.question_contents}</p>
+      </div>
+    )
+  }
+});
+
 var ready = function () {
   ReactDOM.render(
-    <TestDiv />,
+    <Todo />,
     document.getElementById("todo")
   );
 };
