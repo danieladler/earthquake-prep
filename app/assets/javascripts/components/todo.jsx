1f402010fc29a86dd2280eea3544e5375b8e0f5f
danieladler <weston.adler@gmail.com>
2016-03-22 16:51:47 -0700

    show/action pane for preps is displayed after prep is clicked in list

--- app/assets/javascripts/components/todo.jsx
+++ app/assets/javascripts/components/todo.jsx
@@ -8,15 +8,39 @@ var Todo = React.createClass({
   getInitialState: function() {
     return {preps: preps, currentPrep: null}
   },
-  setCurrentTodo: function(prep) {
+  setCurrentPrep: function(prep) {
     this.setState({currentPrep: prep})
   },
   render: function() {
     return(
       <div>
-        <PrepTypeList preps={this.state.preps}/>
+        <PrepTypeList handleClick={this.setCurrentPrep} preps={this.state.preps}/>
+        <ShowPrep prep={this.state.currentPrep}/>
       </div>
     );
+  }
+});
+
+var ShowPrep = React.createClass({
+  render: function() {
+    var body;
+    if(this.props.prep) {
+      body = (
+        <div>
+          <h2>{this.props.prep.prep_type} Preparation:</h2>
+          <h3>{this.props.prep.question_contents}</h3>
+          <p>Notes:</p>
+          <button>Complete & Save</button>
+        </div>
+      )
+    } else {
+      body = <h2> No Prep Selected </h2>
+    }
+    return(
+      <div className="show-prep">
+        {body}
+      </div>
+    )
   }
 });
 
@@ -25,7 +49,7 @@ var PrepTypeList = React.createClass({
     var prepDivs = [];
 
     for (var i = 0; i < this.props.preps.length; i++) {
-      prepDivs.push(<PrepItem key={i} prep={this.props.preps[i]}/>)
+      prepDivs.push(<PrepItem handleClick={this.props.handleClick} key={i} prep={this.props.preps[i]}/>)
     }
 
     return(
@@ -38,9 +62,12 @@ var PrepTypeList = React.createClass({
 });
 
 var PrepItem = React.createClass({
+  showPrep: function() {
+    this.props.handleClick(this.props.prep);
+  },
   render: function() {
     return(
-      <div>
+      <div onClick={this.showPrep} className="prep-item">
         <p>{this.props.prep.question_contents}</p>
       </div>
     )
