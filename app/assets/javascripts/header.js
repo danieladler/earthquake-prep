5c3da7c98e5ebffa690f433eeb7c4a406170f5c6
danieladler <weston.adler@gmail.com>
2016-03-26 10:21:25 -0700

    site-wide colors of backgrounds, fields, borders, etc.

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-
-  var headerWrap = $(".header-wrap");
-  var headerHeight = headerWrap.outerHeight();
-
-  $(document).scroll(function(e){
-    var scrollPercent = (headerHeight - window.scrollY) / headerHeight;
-    // console.log(scrollPercent);
-      if (scrollPercent < .70) {
-        headerWrap.css('background-color', 'rgba(145, 145, 145, 1)');
-      }
-      else  headerWrap.css('background-color', 'rgba(255, 255, 255, 0.0)');
-  });
-});
+// $(document).ready(function () {
+//
+//   var headerWrap = $(".header-wrap");
+//   var headerHeight = headerWrap.outerHeight();
+//
+//   $(document).scroll(function(e){
+//     var scrollPercent = (headerHeight - window.scrollY) / headerHeight;
+//     // console.log(scrollPercent);
+//       if (scrollPercent < .70) {
+//         headerWrap.css('background-color', 'rgba(145, 145, 145, 1)');
+//       }
+//       else  headerWrap.css('background-color', 'rgba(255, 255, 255, 0.0)');
+//   });
+// });
