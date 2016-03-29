0c8f35d5591c315c9470e2cbe6d18bb923b9b843
danieladler <weston.adler@gmail.com>
2016-03-29 09:14:31 -0700

    fix values in home_asmt_form and start detailing gear_asmt_form pre-todo checkoff options

--- app/controllers/gear_assessment_controller.rb
+++ app/controllers/gear_assessment_controller.rb
@@ -1,11 +1,17 @@
 class GearAssessmentController < ApplicationController
   def update_gear
+    # raise
     gear_asmt_checks(current_user)
     render nothing: true
   end
 
   def gear_asmt_checks(user)
     @ga = GearAssessment.new(user)
+    # create preps for the 3-4 things asked in the form
+    form_keywords = %(radio clothes)
+    @ga.create_and_check()
+    # raise
+    # end preliminary prep creation from form
     @ga.run_all_checks
   end
 end
